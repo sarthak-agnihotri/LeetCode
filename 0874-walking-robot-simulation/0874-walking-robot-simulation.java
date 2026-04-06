f2682ecf9e61cf7aeb7442c052c3b9d1643dac83
Sarthak Agnihotri <149502807+sarthak-agnihotri@users.noreply.github.com>
2026-04-06 10:30:20 +0530

    Time: 35 ms (52.9%), Space: 51.9 MB (68.84%) - LeetHub

---
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int robotSim(int[] commands, int[][] obstacles) {
+        // Store obstacles in a set for O(1) lookup
+        Set<String> obstacleSet = new HashSet<>();
+        for (int[] obs : obstacles) {
+            obstacleSet.add(obs[0] + "," + obs[1]);
+        }
+
+        // Directions: North, East, South, West
+        int[][] dirs = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        int dir = 0; // start facing North
+
+        int x = 0, y = 0;
+        int maxDist = 0;
+
+        for (int cmd : commands) {
+            if (cmd == -1) {
+                dir = (dir + 1) % 4; // turn right
+            } else if (cmd == -2) {
+                dir = (dir + 3) % 4; // turn left
+            } else {
+                for (int i = 0; i < cmd; i++) {
+                    int nx = x + dirs[dir][0];
+                    int ny = y + dirs[dir][1];
+
+                    if (obstacleSet.contains(nx + "," + ny)) {
+                        break; // stop if obstacle
+                    }
+
+                    x = nx;
+                    y = ny;
+                    maxDist = Math.max(maxDist, x * x + y * y);
+                }
+            }
+        }
+
+        return maxDist;
+    }
+}
