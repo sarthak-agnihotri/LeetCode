4ba762d79f9467f7d16b42ce1415813fd114a19e
Sarthak Agnihotri <149502807+sarthak-agnihotri@users.noreply.github.com>
2026-06-25 16:49:05 +0530

    Time: 0 ms (100%), Space: 45.3 MB (11.81%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,11 +1,49 @@
+// class Solution {
+//     public boolean search(int[] nums, int target) {
+//         int n=nums.length;
+//         for(int i=0;i<n;i++){
+//             if(nums[i]==target){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
 class Solution {
     public boolean search(int[] nums, int target) {
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            if(nums[i]==target){
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
                 return true;
             }
+            if (nums[left] == nums[mid]
+                    && nums[mid] == nums[right]) {
+                left++;
+                right--;
+            }
+            else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target
+                        && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            else {
+                if (nums[mid] < target
+                        && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
         }
+
         return false;
     }
 }
