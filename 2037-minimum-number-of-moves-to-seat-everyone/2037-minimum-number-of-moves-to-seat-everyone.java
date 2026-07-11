eb3cdc4a67a9a68291177dc4e9aace7aea5be483
Sarthak Agnihotri <149502807+sarthak-agnihotri@users.noreply.github.com>
2026-07-11 13:21:10 +0530

    Time: 1 ms (100%), Space: 46.3 MB (93.42%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.java
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.java
@@ -1,12 +1,28 @@
 class Solution {
     public int minMovesToSeat(int[] seats, int[] students) {
-        Arrays.sort(seats);
-        Arrays.sort(students);
-        int minmoves=0;
-        int n=seats.length;
-        for(int i=0;i<n;i++){
-            minmoves+=Math.abs(seats[i]-students[i]);
+        int[] seatFreq=new int[101];
+        int[] studentFreq=new int[101];
+
+        for(int seat: seats){
+            seatFreq[seat]++;
         }
-        return minmoves;
+
+        for(int student: students){
+            studentFreq[student]++;
+        }
+
+        int moves=0;
+        int j=1;
+        for(int i=1;i<=100;i++){
+            while(seatFreq[i]>0){
+                while(studentFreq[j]==0){
+                    j++;
+                }
+                moves+=Math.abs(j-i);
+                seatFreq[i]--;
+                studentFreq[j]--;
+            }
+        }
+        return moves;
     }
 }
