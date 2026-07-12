e633cc1cd286925a88ca5a64632141d142b25318
Sarthak Agnihotri <149502807+sarthak-agnihotri@users.noreply.github.com>
2026-07-12 11:14:30 +0530

    Time: 0 ms (100%), Space: 43.5 MB (92.35%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.java
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -1,37 +1,27 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        HashMap<Integer,Integer>map=new HashMap<>();
-        for(int num: arr1){
-            if(map.containsKey(num)){
-                map.put(num,map.get(num)+1);
-            }else{
-                map.put(num,1);
-            }
+        int[] count=new int[1001];
+        for(int num:arr1){
+            count[num]++;
         }
-        int[] result=new int[arr1.length];
+        int[] ans=new int[arr1.length];
         int index=0;
+
         for(int num:arr2){
-            if(map.containsKey(num)){
-                int count=map.get(num);
-                while(count>0){
-                    result[index++]=num;
-                    count--;
-                }
-                map.remove(num);
+            while(count[num]>0){
+                ans[index]=num;
+                index++;
+                count[num]--;
             }
         }
-        ArrayList<Integer>remaining=new ArrayList<>();
-        for(int key:map.keySet()){
-            int count=map.get(key);
-            while(count>0){
-                remaining.add(key);
-                count--;
+
+        for(int i=0;i<count.length;i++){
+            while(count[i]>0){
+                ans[index]=i;
+                index++;
+                count[i]--;
             }
         }
-        Collections.sort(remaining);
-        for(int num: remaining){
-            result[index++]=num;
-        }
-        return result;
+        return ans;
     }
 }
