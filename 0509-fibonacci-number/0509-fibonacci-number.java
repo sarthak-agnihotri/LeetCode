7850f9753399985cf268aaca3dcb5ee5e7148519
Sarthak Agnihotri <149502807+sarthak-agnihotri@users.noreply.github.com>
2026-07-28 15:20:38 +0530

    Time: 0 ms (100%), Space: 42.2 MB (15.86%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,21 +1,17 @@
 class Solution {
-    int[] dp = new int[31];
-
     public int fib(int n) {
-        java.util.Arrays.fill(dp, -1);
-        return solve(n);
-    }
-
-    private int solve(int n) {
         if (n <= 1) {
             return n;
         }
 
-        if (dp[n] != -1) {
-            return dp[n];
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
 
-        dp[n] = solve(n - 1) + solve(n - 2);
         return dp[n];
     }
 }
