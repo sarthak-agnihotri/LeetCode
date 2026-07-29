1d5487cbd0c0cb27ea65f2c1ca02fb30309370dd
Sarthak Agnihotri <149502807+sarthak-agnihotri@users.noreply.github.com>
2026-07-29 22:36:57 +0530

    Time: 0 ms (100%), Space: 42.2 MB (15.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int solve(int[] dp,int n){
         if(n<=1)return n;
         if(dp[n]!=-1)return dp[n];
-        dp[n]=fib(n-1)+fib(n-2);
+        dp[n]=solve(dp,n-1)+solve(dp,n-2);
         return dp[n];
     }
     public int fib(int n) {
