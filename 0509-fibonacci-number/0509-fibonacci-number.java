ff44f35292f55bd4ff3c2b2ce9c9666831936677
Sarthak Agnihotri <149502807+sarthak-agnihotri@users.noreply.github.com>
2026-07-29 22:35:03 +0530

    Time: 39 ms (9.33%), Space: 46.3 MB (15.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,10 +1,13 @@
 class Solution {
-    public int fib(int n) {
+    public int solve(int[] dp,int n){
         if(n<=1)return n;
-        int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
         if(dp[n]!=-1)return dp[n];
         dp[n]=fib(n-1)+fib(n-2);
         return dp[n];
+    }
+    public int fib(int n) {
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(dp,n);
     }
 }
