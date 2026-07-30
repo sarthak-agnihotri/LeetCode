b4a30701a0ed55ca8fb47584b2f5e16f3eeb18cf
Sarthak Agnihotri <149502807+sarthak-agnihotri@users.noreply.github.com>
2026-07-30 19:51:22 +0530

    Time: 0 ms (100%), Space: 42.4 MB (97.62%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,15 @@
 class Solution {
-    public int solve(int[] nums,int i,int[]dp){
-        if(i>=nums.length)return 0;
-        if(dp[i]!=-1)return dp[i];
-        int steal=nums[i]+solve(nums,i+2,dp);
-        int skip=solve(nums,i+1,dp);
-        return dp[i]=Math.max(steal,skip);
-    }
     public int rob(int[] nums) {
         int n=nums.length;
         int[]dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return solve(nums,0,dp);
+        Arrays.fill(dp,0);
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=2;i<=n;i++){
+            int steal=nums[i-1]+dp[i-2];
+            int skip=dp[i-1];
+            dp[i]=Math.max(steal,skip);
+        }
+        return dp[n];
     }
 }
