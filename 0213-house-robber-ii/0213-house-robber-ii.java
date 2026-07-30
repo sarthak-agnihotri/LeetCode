75b5098653f79566cbc9bc9f3e44e511d7aa83a7
Sarthak Agnihotri <149502807+sarthak-agnihotri@users.noreply.github.com>
2026-07-30 22:34:44 +0530

    Time: 0 ms (100%), Space: 42.7 MB (61.28%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,44 +1,19 @@
 class Solution {
-
+    public int solve(int[]nums,int start,int end){
+        int prev1=0;
+        int prev2=0;
+        for(int i=start;i<=end;i++){
+            int curr=Math.max(prev1,prev2+nums[i]);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
     public int rob(int[] nums) {
-
-        int n = nums.length;
-
-        // Agar sirf ek house hai
-        if (n == 1)return nums[0];
-
-        int[] dp = new int[n + 1];
-
-        int result1 = 0;
-        int result2 = 0;
-
-        // Case 1: First house include ho sakta hai, isliye last house exclude
-        dp[0] = 0;
-
-        for (int i = 1; i <= n - 1; i++) {
-            int skip=dp[i-1];
-            int steal=nums[i-1]+(i - 2 >= 0 ? dp[i - 2] : 0);
-            dp[i] = Math.max(skip,steal);
-        }
-
-        result1 = dp[n - 1];
-
-        // DP array ko reset karo
-        dp = new int[n + 1];
-
-        // Case 2: First house exclude, isliye last house include ho sakta hai
-        dp[0] = 0;
-        dp[1] = 0;
-
-        for (int i = 2; i <= n; i++) {
-            int skip=dp[i-1];
-            int steal=nums[i-1]+(i - 2 >= 0 ? dp[i - 2] : 0);
-            dp[i] = Math.max(skip,steal);
-        }
-
-        result2 = dp[n];
-
-        // Dono cases me se maximum answer return karo
-        return Math.max(result1, result2);
+        int n=nums.length;
+        if(n==1)return nums[0];
+        int ans1=solve(nums,0,n-2);
+        int ans2=solve(nums,1,n-1);
+        return Math.max(ans1,ans2);
     }
 }
