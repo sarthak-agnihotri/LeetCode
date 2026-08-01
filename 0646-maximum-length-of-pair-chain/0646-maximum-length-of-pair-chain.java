f8afcaef27297c7bc9a76449c4d2891e55c15949
Sarthak Agnihotri <149502807+sarthak-agnihotri@users.noreply.github.com>
2026-08-01 12:09:34 +0530

    Time: 31 ms (23.44%), Space: 47.1 MB (32.69%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -1,57 +1,18 @@
 class Solution {
-
-    // curr = current index
-    // prev = last selected pair ka index (-1 matlab abhi tak koi pair select nahi hua)
-    public int solve(int curr, int prev, int[][] pairs, int[][] dp) {
-
-        // Base case: saare pairs dekh liye
-        if (curr == pairs.length)
-            return 0;
-
-        // Agar answer pehle se calculate hai to direct return karo
-        if (dp[curr][prev + 1] != -1)
-            return dp[curr][prev + 1];
-
-        // Option 1: Current pair ko skip karo
-        int skip = solve(curr + 1, prev, pairs, dp);
-
-        // Option 2: Current pair ko lo (agar valid hai)
-        int take = 0;
-
-        // Agar koi previous pair nahi hai
-        // ya current pair ka start, previous pair ke end se bada hai
-        // to chain continue ho sakti hai
-        if (prev == -1 || pairs[curr][0] > pairs[prev][1]) {
-
-            // 1 current pair ke liye
-            // + baaki answer recursion se
-            take = 1 + solve(curr + 1, curr, pairs, dp);
-        }
-
-        // Take aur Skip me se maximum answer store karo
-        return dp[curr][prev + 1] = Math.max(take, skip);
-    }
-
     public int findLongestChain(int[][] pairs) {
-
-        // First element ke basis par sorting
-        // Taaki recursion left se right easily chale
-        Arrays.sort(pairs, (a, b) -> Integer.compare(a[0], b[0]));
-
-        int n = pairs.length;
-
-        // dp[curr][prev+1]
-        // prev+1 isliye kyunki prev = -1 bhi ho sakta hai
-        int[][] dp = new int[n][n + 1];
-
-        // DP ko -1 se initialize karo (matlab abhi answer calculate nahi hua)
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        Arrays.sort(pairs,(a,b)->Integer.compare(a[0],b[0]));
+        int n=pairs.length;
+        int[]dp=new int[n];
+        Arrays.fill(dp,1);
+        int ans=1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(pairs[i][0]>pairs[j][1]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                }
+            }
+            ans=Math.max(ans,dp[i]);
         }
-
-        // Start recursion
-        // curr = 0
-        // prev = -1 (abhi tak koi pair choose nahi kiya)
-        return solve(0, -1, pairs, dp);
+        return ans;
     }
 }
