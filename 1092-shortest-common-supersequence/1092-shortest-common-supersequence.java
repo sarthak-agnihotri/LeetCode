16ad7c4e9f726cd96af7db2c28d890681928be09
Sarthak Agnihotri <149502807+sarthak-agnihotri@users.noreply.github.com>
2026-08-12 19:19:19 +0530

    Time: 23 ms (68.84%), Space: 54 MB (50.16%) - LeetHub

---
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -0,0 +1,70 @@
+class Solution {
+    public String shortestCommonSupersequence(String str1, String str2) {
+
+        int n = str1.length();
+        int m = str2.length();
+
+        // dp[i][j] = LCS length
+        int[][] dp = new int[n + 1][m + 1];
+
+        // Build DP table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j],dp[i][j - 1]);
+                }
+            }
+        }
+
+        // Construct answer
+        StringBuilder ans = new StringBuilder();
+
+        int i = n;
+        int j = m;
+
+        while (i > 0 && j > 0) {
+
+            // Same character
+            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+
+                ans.append(str1.charAt(i - 1));
+
+                i--;
+                j--;
+            }
+
+            // Move up
+            else if (dp[i - 1][j] >= dp[i][j - 1]) {
+
+                ans.append(str1.charAt(i - 1));
+
+                i--;
+            }
+
+            // Move left
+            else {
+
+                ans.append(str2.charAt(j - 1));
+
+                j--;
+            }
+        }
+
+        // Remaining characters of str1
+        while (i > 0) {
+            ans.append(str1.charAt(i - 1));
+            i--;
+        }
+
+        // Remaining characters of str2
+        while (j > 0) {
+            ans.append(str2.charAt(j - 1));
+            j--;
+        }
+
+        return ans.reverse().toString();
+    }
+}
