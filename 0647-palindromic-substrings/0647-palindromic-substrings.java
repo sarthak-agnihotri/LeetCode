dba093d74b78a6ec005a22421185b1fd01adc5c4
Sarthak Agnihotri <149502807+sarthak-agnihotri@users.noreply.github.com>
2026-09-01 12:29:22 +0530

    Time: 29 ms (25.1%), Space: 61.4 MB (5.73%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,19 +1,34 @@
 class Solution {
-    public boolean check(String s, int i, int j){
-        if(i>j)return true;
-        if(s.charAt(i)==s.charAt(j)) return(check(s,i+1,j-1));
-        return false;
+    Boolean[][] dp;
+
+    public boolean check(String s, int i, int j) {
+        if (i >= j) return true;
+
+        if (dp[i][j] != null) {
+            return dp[i][j];
+        }
+
+        if (s.charAt(i) == s.charAt(j)) {
+            return dp[i][j] = check(s, i + 1, j - 1);
+        }
+
+        return dp[i][j] = false;
     }
+
     public int countSubstrings(String s) {
-        int len=s.length();
-        int count=0;
-        for(int i=0;i<len;i++){
-            for(int j=i;j<len;j++){
-                if(check(s,i,j)){
+        int len = s.length();
+        int count = 0;
+
+        dp = new Boolean[len][len];
+
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j < len; j++) {
+                if (check(s, i, j)) {
                     count++;
                 }
             }
         }
+
         return count;
     }
 }
